2cffdfcceff0264db2a02411726ea0b87f6e2c47
Logan Woodbury <lwoodbury18@gmail.com>
2025-05-07 12:52:46 -0500

    journal styling ??

--- dnd-react-app/src/components/JournalEntryComponent/JournalEntryComponent.jsx
+++ dnd-react-app/src/components/JournalEntryComponent/JournalEntryComponent.jsx
@@ -1,12 +1,15 @@
 
+import styles from './JournalEntryComponent.module.css';
+
 
 export default function JournalEntryComponent({ entry }) {
 
     return (
-        <div className="journal-entry">
+        <div className={styles.journalEntry}>
             <h2>{entry.title}</h2>
-            <p>{entry.description}</p>
-            <p><strong>Date:</strong> {new Date(entry.entry_date).toLocaleDateString()}</p>
+            <p className={styles.date}><strong>Date:</strong> {new Date(entry.entry_date).toLocaleDateString()}</p>
+
+            <p className={styles.content}>{entry.description}</p>
         </div>
     );
 }
