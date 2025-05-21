38423aeed6d0f7666a8ecdb9767d3186a03140cc
Logan Woodbury <lwoodbury18@gmail.com>
2025-05-20 20:33:19 -0500

    grid added to drawing area

--- dnd-react-app/src/components/BoardComponent/BoardComponent.jsx
+++ dnd-react-app/src/components/BoardComponent/BoardComponent.jsx
@@ -5,11 +5,21 @@ export default function BoardComponent({lineColor, lineWidth}) {
     const canvasRef = useRef(null);
     const ctxRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
+
+     // Define default grid properties
+     const gridColor = '#e0e0e0'; // A light gray for the grid
+     const gridLineWidth = 1;     // Thin lines for the grid
     
 
     useEffect(() => {
         loadBoard();
-        
+        drawGrid(gridLineWidth, 20, 20, gridColor);
+    }, []);
+    
+    
+    useEffect(() => {
+    
+
     }, [lineColor, lineWidth]);
 
     function loadBoard(){
@@ -28,11 +38,14 @@ export default function BoardComponent({lineColor, lineWidth}) {
         setIsDrawing(true);
         ctxRef.current.beginPath();
         ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        ctxRef.current.strokeStyle = lineColor;
+        ctxRef.current.lineWidth = lineWidth;
     }
 
     function stopDrawing(){
         setIsDrawing(false);
         ctxRef.current.closePath();
+        ctxRef.current.save();
     }
     function draw(e){
         if (!isDrawing) return;
@@ -44,6 +57,36 @@ export default function BoardComponent({lineColor, lineWidth}) {
     function text(e){
         ctxRef.current.fillText(e.target.value, e.nativeEvent.offsetX, e.nativeEvent.offsetY);
     }
+
+    function drawGrid(lineWidth, cellWidth, cellHeight, color){
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        console.log("drawing grid");
+
+        //get size
+        let width = canvas.width;
+        let height = canvas.height;
+
+        //draw vertical lines
+        for(let x = 0; x <=width; x += cellWidth){
+            ctx.beginPath();
+            ctx.moveTo(x,0);
+            ctx.lineTo(x, height);
+            ctx.stroke();
+        }
+
+        for(let y = 0; y <= height; y += cellHeight){
+            ctx.beginPath();
+            ctx.moveTo(0,y);
+            ctx.lineTo(width, y);
+            ctx.stroke();
+        }
+
+        ctx.save();
+    }    
+
 
     return (
         <>
